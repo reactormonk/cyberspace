a0ed87e01f82e2354b2eb768a964aec73641c1df
Simon Hafner <hafnersimon@gmail.com>
2010-05-02 22:26:09 +0200

    refined JSONProtocol (doc and so on)

--- lib/cyberspace/json_protocol.rb
+++ lib/cyberspace/json_protocol.rb
@@ -1,29 +1,24 @@
 require 'json'
 module Cyberspace
-  # JSON Data is received as following:
-  #
-  # action  => <method name>
-  # data    => <parameters>
-  #
-  # JSON Data is sent as following:
-  #
-  # status => <error, ok>
-  # data   => ...
-  #
-  # If an exception occured, the data will be sent as:
-  # error     => e.class
-  # body      => e
-  # backtrace => e.backtrace
-  # sent      => json         # incoming data
-  #
+
+  # The Protocol is basically JSON hashes separated by zero bytes.
   module JSONProtocol
 
     private
 
+    # @see EventMachine::Connection#receive_data
+    # EM sends incoming data to this method, it's being parsed and rescued
+    # here. The method passes the parsed hash to {#receive_hash}, rescing to
+    #
+    # error     => e.class
+    # body      => e
+    # backtrace => e.backtrace
+    # sent      => json         # data that caused the error
+    #
     def receive_data(data)
       (@buffer ||= BufferedTokenizer.new("\x00")).extract(data).each do |json|
         begin
-          receive_hash(JSON.parse(json))
+          send_hash(receive_hash(JSON.parse(json)))
         rescue => e
           send_hash({:status => :error, :data => {:error => e.class, :body => e, :backtrace => e.backtrace, :sent => json}})
         end
@@ -32,14 +27,18 @@ module Cyberspace
 
     attr :receiver
 
+    # @raise [NoMethodError] in case the specified action isn't defined
+    # @return [Object] the return value of the called action
     def receive_hash(hash)
-      if (receiver ||= self).respond_to?(hash['action']) && ! Object.new.respond_to?(hash['action'])
+      if (receiver ||= self).respond_to?(hash['action']) && ! Object.method_defined?(hash['action'])
         receiver.send(hash['action'])
       else
         raise NoMethodError, "private method called: #{hash['action']}"
       end
     end
 
+    # @param [#to_json]
+    # @see EventMachine::Connection#send_data
     def send_hash(hash)
       send_data(hash.to_json + "\x00")
     end
