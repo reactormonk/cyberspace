348a7674a65a8a1238396187c2e33e0a9afc0d5b
Simon Hafner <hafnersimon@gmail.com>
2010-05-10 16:48:25 +0200

    fixed ruby jail typos

--- lib/cyberspace/jails/ruby.rb
+++ lib/cyberspace/jails/ruby.rb
@@ -14,13 +14,13 @@ module Cyberspace
       # The EM connection
       attr_reader :connection
 
-      # connect the client, and let it send back {:load => :finished} when
+      # connect the client, and let it send back {:action => ready} when
       # all code is loaded, so the game can start
       def enter_the_matrix
         self.connection = EM::popen("ruby #{@libs.map {|lib| "-r#{lib} "}} -T3", @server)
         connection.send_data(@code)
         connection.send_data <<-CODE
-          puts {:action => ready}.to_json + "\x00"
+          puts {:action => :ready}.to_json + "\x00"
           __END__
         CODE
       end
